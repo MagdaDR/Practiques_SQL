be8647a7896b0453d88e061fd87d34b0bfd24e1b
MagdaDR <magda.dalmau@gmail.com>
2019-04-15 12:59:23 +0200

    Pujar la pràctica de SQL queries

---
+++ ExerciciQueriesSQL/ExerQueriesSQL.sql
@@ -0,0 +1,44 @@
+/* Ex1 */
+SELECT count(*) as totalFlights FROM usairlineflights2.flights;
+
+/*Ex2*/
+SELECT flights.origin, avg(flights.ArrDelay) as prom_arribades , avg(DepDelay) as prom_Ret_Sortides FROM flights group by flights.origin;
+
+/*Ex3*/
+SELECT flights.Origin, flights.colYear, flights.colMonth, avg(flights.ArrDelay) as prom_Ret_Sortides FROM flights  group by flights.Origin, flights.colYear, flights.colMonth order by Origin;
+
+/*Ex4*/
+SELECT usairports.Airport, flights.colYear, flights.colMonth, avg(flights.ArrDelay) as prom_Ret_Sortides FROM flights, usairports  group by flights.Origin, flights.colYear, flights.colMonth order by flights.Origin, flights.colYear, flights.colMonth;
+
+/*Ex5*/  
+SELECT carriers.CarrierCode, flights.colYear, flights.colMonth, avg(flights.ArrDelay) as prom_Ret_Sortides,
+		sum(flights.Cancelled) as total_cancelled
+	FROM flights, carriers
+    where flights.Cancelled=1 and carriers.CarrierCode=flights.UniqueCarrier
+    group by flights.Origin, flights.colYear, flights.colMonth 
+    order by flights.Origin, flights.colYear, flights.colMonth;
+
+/*Ex6*/
+select flights.tailnum, SUM(flights.distance) as total_distance
+	from flights
+     where flights.tailnum<>'' 
+      group by flights.TailNum 
+    order by total_distance desc
+    limit 10
+    ;
+
+    
+/*Ex7*/
+/*select carriers.CarrierCode, avg(flights.ArrDelay) as prom_Ret_Sortides	
+	FROM flights, carriers  
+    where carriers.CarrierCode=flights.UniqueCarrier
+    group by carriers.CarrierCode
+    ;  */
+select carriers.CarrierCode, avg(flights.ArrDelay) as prom_Ret_Sortides	
+	FROM flights, carriers 
+    where carriers.CarrierCode=flights.UniqueCarrier 
+    group by carriers.CarrierCode
+    having prom_Ret_Sortides > 10
+    order by prom_Ret_Sortides desc
+    ; 
+
